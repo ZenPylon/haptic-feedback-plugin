21a9108511deb3518f75682ae41a450022bef26b
Andrew Decker <ajdecker1022@gmail.com>
2019-12-11 06:33:17 -0600

    Update iOS plugin code

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -1,3 +1,4 @@
+import CoreHaptics
 import Foundation
 import Capacitor
 
@@ -5,13 +6,37 @@ import Capacitor
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitor.ionicframework.com/docs/plugins/ios
  */
+@available(iOS 13.0, *)
 @objc(HapticFeedbackPlugin)
 public class HapticFeedbackPlugin: CAPPlugin {
+    var engine: CHHapticEngine?
     
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.success([
-            "value": value
-        ])
+    override init () {
+        super.init()
+        do {
+            engine = try CHHapticEngine()
+        }
+        catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+        
     }
+    
+    @objc func start(_ call: CAPPluginCall) {
+        do {
+            try engine?.start()
+        } catch {
+            let message = "There was an error starting the engine: \(error.localizedDescription)"
+            print(message)
+            return call.error(message)
+        }
+    
+        call.success()
+    }
+    
+    @objc func stop(_ call: CAPPluginCall) {
+        engine?.stop()
+        call.success()
+    }
+    
 }
